1aaf757712f688da9e23a99f42b390759c32e6dd
Lasaad BEN ZAIED <lasaad.benzaied@gmail.com>
2020-02-05 21:53:58 +0100

    Add: Unit Test

---
+++ MathsProgression/MathsProgression/MathsProgression.Tests/Utilities Unit Test/ConvertUnitTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathsProgression.Tests
+{
+    [TestClass]
+    public class ConvertUnitTest
+    {
+        const string XMLsimpleTag = "<foo>bar</foo>",
+            XMLcomplexTag = "<TRANS><HPAY><ID>103</ID><STATUS>3</STATUS><EXTRA><IS3DS>0</IS3DS><AUTH>031183</AUTH></EXTRA><INT_MSG/><MLABEL>501767XXXXXX6700</MLABEL><MTOKEN>project01</MTOKEN></HPAY></TRANS>",
+            XMLbadFormat = "<foo>hello</bar>",
+            JSONsimpleTag = "{\"foo\":\"bar\"}",
+            JSONcomplexTag = "{\"TRANS\":{\"HPAY\":{\"ID\":\"103\",\"STATUS\":\"3\",\"EXTRA\":{\"IS3DS\":\"0\",\"AUTH\":\"031183\"},\"INT_MSG\":null,\"MLABEL\":\"501767XXXXXX6700\",\"MTOKEN\":\"project01\"}}}",
+            JSONbadFormat = "Bad Xml format";
+        
+
+        [TestMethod]
+        public void XmlToJsonTest()
+        {
+            var resultat = Convert.XmlToJson(XMLsimpleTag);
+            Assert.AreEqual(JSONsimpleTag, resultat, "Incorrect JSON");
+
+            resultat = Convert.XmlToJson(XMLcomplexTag);
+            Assert.AreEqual(JSONcomplexTag, resultat, "Incorrect JSON");
+
+            resultat = Convert.XmlToJson(XMLbadFormat);
+            Assert.AreEqual(JSONbadFormat, resultat, "Incorrect JSON");
+
+        }
+    }
+}
---
+++ MathsProgression/MathsProgression/MathsProgression.Tests/Utilities Unit Test/MathProgressionUnitTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using MathsProgression.Utilities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MathsProgression.Tests
+{
+    [TestClass]
+    public class MathProgressionTest
+    {
+        [TestMethod]
+        public void FibonacciTest()
+        {
+            int resultat = MathsUtilities.Fibonacci(0, 1, 1, 5); 
+            Assert.AreEqual(5, resultat, "method must return 5 ");
+            
+            resultat = MathsUtilities.Fibonacci(0, 1, 1, 6); 
+            Assert.AreEqual(8, resultat, "method must return 8 ");
+
+            resultat = MathsUtilities.Fibonacci(0, 1, 1, 35);
+            Assert.AreEqual(9227465, resultat, "method must return 9 227 465 ");
+
+        }
+    }
+}
