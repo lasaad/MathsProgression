cb52766c242fa03d1bb39e47a65c42acfc8f7491
Lasaad BEN ZAIED <lasaad.benzaied@gmail.com>
2020-02-05 17:36:38 +0100

    Add : Log4net config

--- MathsProgression/MathsProgression/MathsProgression/Properties/AssemblyInfo.cs
+++ MathsProgression/MathsProgression/MathsProgression/Properties/AssemblyInfo.cs
@@ -33,3 +33,4 @@ using System.Runtime.InteropServices;
 // en utilisant '*', comme indiqué ci-dessous :
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: log4net.Config.XmlConfigurator(ConfigFile = "log4net.config")]
