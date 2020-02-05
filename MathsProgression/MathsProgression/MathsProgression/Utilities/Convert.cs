392cb91e310d0a8383abe03d332bdd04c387f0a8
Lasaad BEN ZAIED <lasaad.benzaied@gmail.com>
2020-02-05 15:40:29 +0100

    Add Implementation of Convert Class and MathsUtilities

---
+++ MathsProgression/MathsProgression/MathsProgression/Utilities/Convert.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Xml;
+using Newtonsoft.Json;
+
+namespace MathsProgression
+{
+    /// <summary>
+    /// Class contains  static methods for convert data on specific format 
+    /// </summary>
+    /// 
+    public static class Convert
+    {
+        /// <summary>
+        ///Convert XML string to Json string
+        /// </summary>
+        /// <param name="xml">xml string</param>
+        /// <returns>json string</returns>
+        public static string XmlToJson(string xml)
+        {
+            XmlDocument doc = new XmlDocument();
+            string jsonFormat = "";
+
+            doc.LoadXml(xml);
+            jsonFormat = JsonConvert.SerializeXmlNode(doc);
+
+            return jsonFormat;
+        }
+    }
+}
---
+++ MathsProgression/MathsProgression/MathsProgression/Utilities/MathsUtilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MathsProgression.Utilities
+{
+    public class MathsUtilities
+    {
+        /// <summary>
+        /// This method return <number>th rank of Fibonacci numbers
+        /// This is a recursive method, please modify only last parameter
+        /// usage : MathsUtilities.Fibonacci(0, 1, 1, rank desired);
+        /// </summary>
+        /// <param name="a">keep 0</param>
+        /// <param name="b">keep 1</param>
+        /// <param name="counter">keep 1</param>
+        /// <param name="number">rank desired</param>
+        /// <returns></returns>
+        public static int Fibonacci(int a, int b, int counter, int number)
+        {
+            if (counter < number + 1)
+                return Fibonacci(b, a + b, counter + 1, number);
+            else
+                return a;
+        }
+    }
+}
