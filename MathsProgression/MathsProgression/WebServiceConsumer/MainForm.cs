97e3ec138a59b62358a4587dc77b7d394fa9d40d
Lasaad BEN ZAIED <lasaad.benzaied@gmail.com>
2020-02-10 15:59:34 +0100

    Rename of class Mainform

--- MathsProgression/MathsProgression/WebServiceConsumer/MainForm.cs
+++ MathsProgression/MathsProgression/WebServiceConsumer/MainForm.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 using log4net;
 using MathsProgression.Services;
 using Newtonsoft.Json.Linq;
@@ -23,6 +24,11 @@ namespace WebServiceConsumer
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Function call when the button is clicked 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Compute_Click(object sender, EventArgs e)
         {
             string resultXML = "",
@@ -32,8 +38,8 @@ namespace WebServiceConsumer
             try
             {
                 var connexion = SOAPConnect.Instance(_url);
-
-                resultXML = connexion.CallWebService();
+                string envelope = connexion.CreateSoapEnvelopeFibonacci(10);
+                resultXML = connexion.CallWebService(envelope);
        
                 json = JObject.Parse(MathsProgression.Convert.XmlToJson(resultXML));
                 resultString = json["soap:Envelope"]["soap:Body"]["FibonacciResponse"]["FibonacciResult"].ToString();
